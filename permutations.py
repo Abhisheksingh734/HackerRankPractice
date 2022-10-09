04c6ac1e585455c057bce3838e141332ab39963a
Abhisheksingh734 <abhisheksinghasr734@gmail.com>
2022-10-10 00:23:34 +0530

    permuting

--- permutations.py
+++ permutations.py
@@ -1,15 +1,11 @@
-from tkinter import Y
-
-
-x = int(input("enter num"))
-y = int(input("enter num2"))
-# z = int(input("enter num3"))
-# n = int(input("enter num4"))
-
-ls=[]
-    
-for i in range(x):
-    for j in range(y):
-        ls.append(i)
-        
-print(ls)
+N=int(input("enter"))
+list=[]
+for i in range(N):
+    for j in range(N):
+        list.insert(i,j)
+        print(list)
+        list.remove(j)
+        list.append(j)
+        list.sort()
+        list.pop()
+        list.reverse()
