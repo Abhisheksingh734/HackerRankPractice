16f7c61ee562c817612a0ba88e603bdf640b3284
Abhisheksingh734 <abhisheksinghasr734@gmail.com>
2022-10-09 13:19:02 +0530

    Incomplete work

---
+++ permutations.py
@@ -0,0 +1,15 @@
+from tkinter import Y
+
+
+x = int(input("enter num"))
+y = int(input("enter num2"))
+# z = int(input("enter num3"))
+# n = int(input("enter num4"))
+
+ls=[]
+    
+for i in range(x):
+    for j in range(y):
+        ls.append(i)
+        
+print(ls)
